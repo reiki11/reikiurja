a28e1d0c5db725b0400e88c1e0d9bdaea8064a76
reiki11 <rajeev.kukreja.jio@gmail.com>
2024-08-09 17:41:43 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
